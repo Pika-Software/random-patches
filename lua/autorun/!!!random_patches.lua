01317e9812dadadeb32bf17c83ddc4595a179a91
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2022-02-06 07:09:15 +0400

    Better `table.Random`
    Thats better then a shitty glua table.Random function

--- lua/autorun/!!!random_patches.lua
+++ lua/autorun/!!!random_patches.lua
@@ -4,4 +4,16 @@ function math.Clamp(inval, minval, maxval)
 	return inval
 end
 
+do
+
+    local math_random = math.random
+    local table_GetKeys = table.GetKeys
+    function table.Random( tab, issequential )
+        local keys = issequential and tab or table_GetKeys(tab)
+        local rand = keys[math_random(1, #keys)]
+        return tab[rand], rand
+    end
+
+end
+
 MsgN( "Random Patches - Game Patched!" )
