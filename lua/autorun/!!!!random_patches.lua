ddf8e65904549ef82bdc2ff0366b989503e5b016
PrikolMen:-b <44779902+PrikolMen@users.noreply.github.com>
2022-02-08 00:42:33 +0400

    table.Shuffle and Fixes

--- lua/autorun/!!!!random_patches.lua
+++ lua/autorun/!!!!random_patches.lua
@@ -1,18 +1,36 @@
-function math.Clamp(inval, minval, maxval)
+function math.Clamp( inval, minval, maxval )
 	if (inval < minval) then return minval end
 	if (inval > maxval) then return maxval end
 	return inval
 end
 
+local math_random = math.random
+
 do
 
-    local math_random = math.random
     local table_GetKeys = table.GetKeys
     function table.Random( tab, issequential )
         local keys = issequential and tab or table_GetKeys(tab)
         local rand = keys[math_random(1, #keys)]
         return tab[rand], rand
     end
+
+end
+
+do
+
+    local table_Count = table.Count
+    function table.shuffle( tbl )
+        local len = table_Count( tbl )
+        for i = len, 1, -1 do
+            local rand = math_random( len )
+            tbl[i], tbl[rand] = tbl[rand], tbl[i]
+        end
+
+        return tbl
+    end
+
+    table.Shuffle = table.shuffle
 
 end
 
@@ -92,111 +110,111 @@ do
             claimed[ply] = nil
         end)
 
-        return
-    end
+    else
 
-    LVV_VANILLA_VOICE_VOLUME = LVV_VANILLA_VOICE_VOLUME or PLAYER.VoiceVolume
-    local LVV_VANILLA_VOICE_VOLUME = LVV_VANILLA_VOICE_VOLUME
+        LVV_VANILLA_VOICE_VOLUME = LVV_VANILLA_VOICE_VOLUME or PLAYER.VoiceVolume
+        local LVV_VANILLA_VOICE_VOLUME = LVV_VANILLA_VOICE_VOLUME
 
-    local timer_Create = timer.Create
-    local LocalPlayer = LocalPlayer
-    local GetConVar = GetConVar
-    local IsValid = IsValid
+        local timer_Create = timer.Create
+        local LocalPlayer = LocalPlayer
+        local GetConVar = GetConVar
+        local IsValid = IsValid
 
-    local Me
-    timer_Create("LocalVoiceVolume.LocalPlayer", 0, 0, function()
-        if IsValid(LocalPlayer()) then
-            Me = LocalPlayer()
-            timer.Remove("LocalVoiceVolume.LocalPlayer")
-        end
-    end)
-
-    local networkedVoiceVolume = 0
-    local voice_loopback = GetConVar("voice_loopback")
-    local claimed = {}
-
-    function PLAYER:VoiceVolume()
-        if self == Me and not voice_loopback:GetBool() then
-            return networkedVoiceVolume
-        else
-            return LVV_VANILLA_VOICE_VOLUME(self)
-        end
-    end
-
-    local math_min = math.min
-
-    net_Receive("LocalVoiceVolume.Relay", function()
-        networkedVoiceVolume = math_min(net_ReadUInt(voiceVolumeBits) / 100, 100)
-    end)
-
-    local timer_Exists = timer.Exists
-    local timer_Start = timer.Start
-
-    hook_Add("PlayerStartVoice", "LocalVoiceVolume", function(ply)
-        if not Me or ply == Me or ply:IsBot() or ply:GetVoiceVolumeScale() ~= 1 then return end
-
-        local timerEndName = "LocalVoiceVolume.End:" .. ply:AccountID()
-        if timer_Exists(timerEndName) then
-            timer_Start(timerEndName)
-        else
-            net_Start("LocalVoiceVolume.Claim", true)
-                net_WriteUInt(ENTITY_EntIndex(ply), plyBits)
-                net_WriteBool(true)
-                net_WriteUInt(PLAYER_VoiceVolume(ply), voiceVolumeBits)
-            net_SendToServer()
-        end
-    end)
-
-    hook_Add("PlayerEndVoice", "LocalVoiceVolume", function(ply)
-        if Me and ply == Me then
-            networkedVoiceVolume = 0
-        end
-    end)
-
-    local gui_IsConsoleVisible = CLIENT and gui.IsConsoleVisible
-    local gui_IsGameUIVisible = CLIENT and gui.IsGameUIVisible
-    local net_SendToServer = CLIENT and net.SendToServer
-
-    local math_Round = math.Round
-
-    net_Receive("LocalVoiceVolume.Claim", function()
-        local plyId = net_ReadUInt(plyBits)
-        local ply = Entity(plyId)
-        if not ENTITY_IsValid(ply) then return end
-
-        local timerName = "LocalVoiceVolume:" .. ply:AccountID()
-        local timerEndName = "LocalVoiceVolume.End:" .. ply:AccountID()
-
-        timer_Create(timerEndName, 1, 1, function()
-            timer.Remove(timerName)
-            claimed[ply] = nil
-
-            if ENTITY_IsValid(ply) then
-                net_Start("LocalVoiceVolume.Claim")
-                    net_WriteUInt(plyId, plyBits)
-                    net_WriteBool(false)
-                net_SendToServer()
+        local Me
+        timer_Create("LocalVoiceVolume.LocalPlayer", 0, 0, function()
+            if IsValid(LocalPlayer()) then
+                Me = LocalPlayer()
+                timer.Remove("LocalVoiceVolume.LocalPlayer")
             end
         end)
 
-        timer_Create(timerName, voiceVolumePoll, 0, function()
-            if not ENTITY_IsValid(ply) then
-                timer.Remove(timerName)
-                timer.Remove(timerEndName)
-                claimed[ply] = nil
-                return
-            end
+        local networkedVoiceVolume = 0
+        local voice_loopback = GetConVar("voice_loopback")
+        local claimed = {}
 
-            if PLAYER_IsSpeaking(ply) and not gui_IsGameUIVisible() and not gui_IsConsoleVisible() then
+        function PLAYER:VoiceVolume()
+            if self == Me and not voice_loopback:GetBool() then
+                return networkedVoiceVolume
+            else
+                return LVV_VANILLA_VOICE_VOLUME(self)
+            end
+        end
+
+        local math_min = math.min
+
+        net_Receive("LocalVoiceVolume.Relay", function()
+            networkedVoiceVolume = math_min(net_ReadUInt(voiceVolumeBits) / 100, 100)
+        end)
+
+        local timer_Exists = timer.Exists
+        local timer_Start = timer.Start
+
+        hook_Add("PlayerStartVoice", "LocalVoiceVolume", function(ply)
+            if not Me or ply == Me or ply:IsBot() or ply:GetVoiceVolumeScale() ~= 1 then return end
+
+            local timerEndName = "LocalVoiceVolume.End:" .. ply:AccountID()
+            if timer_Exists(timerEndName) then
                 timer_Start(timerEndName)
-
-                net_Start("LocalVoiceVolume.Relay", true)
-                    net_WriteUInt(plyId, plyBits)
-                    net_WriteUInt(math_Round(PLAYER_VoiceVolume(ply) * 100), voiceVolumeBits)
+            else
+                net_Start("LocalVoiceVolume.Claim", true)
+                    net_WriteUInt(ENTITY_EntIndex(ply), plyBits)
+                    net_WriteBool(true)
+                    net_WriteUInt(PLAYER_VoiceVolume(ply), voiceVolumeBits)
                 net_SendToServer()
             end
         end)
-    end)
+
+        hook_Add("PlayerEndVoice", "LocalVoiceVolume", function(ply)
+            if Me and ply == Me then
+                networkedVoiceVolume = 0
+            end
+        end)
+
+        local gui_IsConsoleVisible = CLIENT and gui.IsConsoleVisible
+        local gui_IsGameUIVisible = CLIENT and gui.IsGameUIVisible
+        local net_SendToServer = CLIENT and net.SendToServer
+
+        local math_Round = math.Round
+
+        net_Receive("LocalVoiceVolume.Claim", function()
+            local plyId = net_ReadUInt(plyBits)
+            local ply = Entity(plyId)
+            if not ENTITY_IsValid(ply) then return end
+
+            local timerName = "LocalVoiceVolume:" .. ply:AccountID()
+            local timerEndName = "LocalVoiceVolume.End:" .. ply:AccountID()
+
+            timer_Create(timerEndName, 1, 1, function()
+                timer.Remove(timerName)
+                claimed[ply] = nil
+
+                if ENTITY_IsValid(ply) then
+                    net_Start("LocalVoiceVolume.Claim")
+                        net_WriteUInt(plyId, plyBits)
+                        net_WriteBool(false)
+                    net_SendToServer()
+                end
+            end)
+
+            timer_Create(timerName, voiceVolumePoll, 0, function()
+                if not ENTITY_IsValid(ply) then
+                    timer.Remove(timerName)
+                    timer.Remove(timerEndName)
+                    claimed[ply] = nil
+                    return
+                end
+
+                if PLAYER_IsSpeaking(ply) and not gui_IsGameUIVisible() and not gui_IsConsoleVisible() then
+                    timer_Start(timerEndName)
+
+                    net_Start("LocalVoiceVolume.Relay", true)
+                        net_WriteUInt(plyId, plyBits)
+                        net_WriteUInt(math_Round(PLAYER_VoiceVolume(ply) * 100), voiceVolumeBits)
+                    net_SendToServer()
+                end
+            end)
+        end)
+    end
 
 end
 
