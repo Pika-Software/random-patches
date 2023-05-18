b6b8cd803745ff262536f41b722c91d6d73082bd
PrikolMen <olegis019@gmail.com>
2023-05-18 04:03:59 +0400

    Updated

--- lua/autorun/!!!random_patches.lua
+++ lua/autorun/!!!random_patches.lua
@@ -1,36 +1,29 @@
---[[
-
-	Title: Random Patches
-	Workshop: https://steamcommunity.com/sharedfiles/filedetails/?id=2806290767
-	GitHub: https://github.com/Pika-Software/gmod_random_patches
-
---]]
-
-local addonName = 'Random Patches'
-local version = '3.6.0'
+local addonName = "Random Patches"
+local version = "3.7.0"
+local NULL = NULL
 
 function IsValid( object )
-	if (object == nil) then return false end
-	if (object == false) then return false end
-	if (object == NULL) then return false end
+	if not object then return false end
+	if object == NULL then return false end
 
 	local func = object.IsValid
-	if (func == nil) then
-		return false
-	end
+	if not func then return false end
 
 	return func( object )
 end
 
 local IsValid = IsValid
 local ipairs = ipairs
+local string = string
 local table = table
 local math = math
 local hook = hook
 
+string.StartsWith = string.StartsWith or string.StartWith
+
 function math.Clamp( num, min, max )
-	if (num < min) then return min end
-	if (num > max) then return max end
+	if num < min then return min end
+	if num > max then return max end
 	return num
 end
 
@@ -75,7 +68,7 @@ do
 		return false
 	end
 
-	hook.Add( 'GameContentChanged', addonName .. ' - Improved IsMounted', cacheMounted )
+	hook.Add( "GameContentChanged", addonName .. " - Improved IsMounted", cacheMounted )
 	cacheMounted()
 
 end
@@ -83,15 +76,15 @@ end
 if SERVER then
 
 	-- Normal Deploy Speed
-	RunConsoleCommand( 'sv_defaultdeployspeed', '1' )
+	RunConsoleCommand( "sv_defaultdeployspeed", "1" )
 
 	-- Missing Stuff
 	-- From metastruct code
-	CreateConVar( 'room_type', '0' )
+	CreateConVar( "room_type", "0" )
 	scripted_ents.Register( {
-		['Base'] = 'base_point',
-		['Type'] = 'point'
-	}, 'info_ladder' )
+		["Base"] = "base_point",
+		["Type"] = "point"
+	}, "info_ladder" )
 
 	-- ENTITY metatable optimization
 	-- Based on Earu#7089 ENTITY.__index optimization
@@ -100,7 +93,7 @@ if SERVER then
 
 	--[[
 	do
-		local ENTITY = FindMetaTable( 'Entity' )
+		local ENTITY = FindMetaTable( "Entity" )
 		local entTabMT = { __index = ENTITY }
 
 		local entMetaID = ENTITY.MetaID
@@ -126,9 +119,9 @@ if SERVER then
 			})
 		end
 
-		hook.Add( 'OnEntityCreated', addonName .. ' - ChangeEntMeta', function(ent)
+		hook.Add( "OnEntityCreated", addonName .. " - ChangeEntMeta", function(ent)
 			-- Experimental optimization, can be disabled by convar, report all problems on our github
-			if CreateConVar( 'randpatches_replace_entmeta', '1', FCVAR_ARCHIVE ):GetBool() then
+			if CreateConVar( "randpatches_replace_entmeta", "1", FCVAR_ARCHIVE ):GetBool() then
 				timer.Simple(0, function()
 					if IsValid(ent) and getmetatable(ent) == ENTITY then
 						changeEntMetaTable(ent)
@@ -144,48 +137,50 @@ if SERVER then
 	-- https://i.imgur.com/yPtoNvO.png
 	-- https://i.imgur.com/a0lmB9m.png
 	do
-		local meta = FindMetaTable( 'Player' )
-		if meta.UserID and debug.getinfo( meta.UserID ).short_src == '[C]' then
+
+		local meta = FindMetaTable( "Player" )
+		if meta.UserID and debug.getinfo( meta.UserID ).short_src == "[C]" then
 			RandomPatches_UserID = RandomPatches_UserID or meta.UserID
 
 			function meta:UserID()
 				return self.__UserID or RandomPatches_UserID( self )
 			end
 
-			local function CacheUserID( ply )
+			local function cacheUserID( ply )
 				ply.__UserID = RandomPatches_UserID( ply )
 			end
-			hook.Add( 'PlayerInitialSpawn', addonName .. ' - CacheUserID', CacheUserID, HOOK_MONITOR_HIGH )
-			hook.Add( 'PlayerAuthed', addonName .. ' - CacheUserID', CacheUserID, HOOK_MONITOR_HIGH )
+
+			hook.Add( "PlayerInitialSpawn", addonName .. " - CacheUserID", cacheUserID, HOOK_MONITOR_HIGH )
+			hook.Add( "PlayerAuthed", addonName .. " - CacheUserID", cacheUserID, HOOK_MONITOR_HIGH )
+
 		end
+
 	end
 
-	-- Area portals fix
+	-- Areaportals fix
 	do
 
 		local mapIsCleaning = false
-		hook.Add( 'PreCleanupMap', addonName .. ' - Area Portal Fix', function() mapIsCleaning = true end )
-		hook.Add( 'PostCleanupMap', addonName .. ' - Area Portal Fix', function() mapIsCleaning = false end )
+		hook.Add( "PreCleanupMap", addonName .. " - Areaportal fix", function() mapIsCleaning = true end )
+		hook.Add( "PostCleanupMap", addonName .. " - Areaportal fix", function() mapIsCleaning = false end )
 
 		local doorClasses = {
-			['func_door_rotating'] = true,
-			['prop_door_rotating'] = true,
-			['func_movelinear'] = true,
-			['func_door'] = true
+			["func_door_rotating"] = true,
+			["prop_door_rotating"] = true,
+			["func_movelinear"] = true,
+			["func_door"] = true
 		}
 
 		local ents_FindByClass = ents.FindByClass
-		hook.Add( 'EntityRemoved', addonName .. ' - Area Portal Fix', function( ent )
-			if (mapIsCleaning) then return end
-			if IsValid( ent ) and doorClasses[ ent:GetClass() ] then
+		hook.Add( "EntityRemoved", addonName .. " - Areaportal fix", function( ent )
+			if not mapIsCleaning and doorClasses[ ent:GetClass() ] then
 				local name = ent:GetName()
-				if (name ~= '') then
-					for _, portal in ipairs( ents_FindByClass( 'func_areaportal' ) ) do
-						if portal:GetInternalVariable( 'target' ) == name then
-							portal:SetSaveValue( 'target', '' )
-							portal:Fire( 'open' )
-						end
-					end
+				if name == "" then return end
+
+				for _, portal in ipairs( ents_FindByClass( "func_areaportal" ) ) do
+					if portal:GetInternalVariable( "target" ) ~= name then continue end
+					portal:SetSaveValue( "target", "" )
+					portal:Fire( "open" )
 				end
 			end
 		end )
@@ -194,40 +189,40 @@ if SERVER then
 
 	-- Fixes for prop_vehicle_prisoner_pod, worldspawn (and other not Valid but not NULL entities) damage taking (bullets only)
 	-- Explosive damage only works if is located in front of prop_vehicle_prisoner_pod (wtf?)
-	hook.Add( 'EntityTakeDamage', addonName .. ' - PrisonerFix', function( ent, dmg )
-		if not IsValid( ent ) then return end
-		if ent:IsNPC() then return end
-
-		if ent.AcceptDamageForce or ent:GetClass() == 'prop_vehicle_prisoner_pod' then
+	hook.Add( "EntityTakeDamage", addonName .. " - Prisoner fix", function( ent, dmg )
+		if not IsValid( ent ) or ent:IsNPC() then return end
+		if ent.AcceptDamageForce or ent:GetClass() == "prop_vehicle_prisoner_pod" then
 			ent:TakePhysicsDamage( dmg )
 		end
 	end )
 
-	hook.Add( 'OnFireBulletCallback', addonName .. ' - PrisonerTakeDamage', function( attk, tr, cdmg )
+	hook.Add( "OnFireBulletCallback", addonName .. " - Prisoner damage", function( _, tr, damageInfo )
 		local ent = tr.Entity
-		if (ent ~= NULL) then
-			hook.Run( 'EntityTakeDamage', ent, cdmg )
-		end
+		if not IsValid( ent ) then return end
+		hook.Run( "EntityTakeDamage", ent, damageInfo )
 	end )
 
 	-- Literally garrysmod-requests #1845
-	hook.Add( 'EntityFireBullets', addonName .. ' - BulletCallbackHook', function( ent, data )
-		local old_callback = data.Callback
-		function data.Callback( attk, tr, cdmg )
-			hook.Run( 'OnFireBulletCallback', attk, tr, cdmg )
-			if old_callback then
-				return old_callback( attk, tr, cdmg )
-			end
+	hook.Add( "EntityFireBullets", addonName .. " - Bullet callback", function( ent, data )
+		local oldCallback = data.Callback
+		function data.Callback( ... )
+			hook.Run( "OnFireBulletCallback", ... )
+			if not oldCallback then return end
+			return oldCallback( ... )
 		end
 	end, HOOK_MONITOR_HIGH )
 
 	-- Steam Auth Check
-	hook.Add( 'PlayerInitialSpawn', addonName .. ' - Steam Auth Check', function( ply )
-		-- If lan server, no authentication - no checks (fixes -multirun)
-		if GetConVar( 'sv_lan' ):GetBool() then return end
-		if ply:IsBot() or ply:IsListenServerHost() or ply:IsFullyAuthenticated() then return end
-		ply:Kick( 'Your SteamID wasn\'t fully authenticated, try restart steam.' )
-	end, HOOK_MONITOR_HIGH )
+	do
+
+		local sv_lan = GetConVar( "sv_lan" )
+
+		hook.Add( "PlayerInitialSpawn", addonName .. " - Steam auth check", function( ply )
+			if sv_lan:GetBool() or ply:IsBot() or ply:IsListenServerHost() or ply:IsFullyAuthenticated() then return end
+			ply:Kick( "Your SteamID wasn\'t fully authenticated, try restart steam." )
+		end, HOOK_MONITOR_HIGH )
+
+	end
 
 	-- Pod network fix by Kefta (code_gs#4197)
 	-- Literally garrysmod-issues #2452
@@ -235,37 +230,32 @@ if SERVER then
 
 		local EFL_NO_THINK_FUNCTION = EFL_NO_THINK_FUNCTION
 
-		hook.Add( 'OnEntityCreated', addonName .. ' - Pod Fix', function( veh )
-			if (veh:GetClass() == 'prop_vehicle_prisoner_pod') then
-				veh:AddEFlags( EFL_NO_THINK_FUNCTION )
-			end
+		hook.Add( "OnEntityCreated", addonName .. " - Pod fix", function( veh )
+			if veh:GetClass() ~= "prop_vehicle_prisoner_pod" then return end
+			veh:AddEFlags( EFL_NO_THINK_FUNCTION )
 		end )
 
-		hook.Add( 'PlayerEnteredVehicle', addonName .. ' - Pod Fix', function( _, veh )
-			if (veh:GetClass() == 'prop_vehicle_prisoner_pod') then
-				veh:RemoveEFlags( EFL_NO_THINK_FUNCTION )
-			end
+		hook.Add( "PlayerEnteredVehicle", addonName .. " - Pod fix", function( _, veh )
+			if veh:GetClass() ~= "prop_vehicle_prisoner_pod" then return end
+			veh:RemoveEFlags( EFL_NO_THINK_FUNCTION )
 		end )
 
-		hook.Add( 'PlayerLeaveVehicle', addonName .. ' - Pod Fix', function( _, veh )
-			if (veh:GetClass() == 'prop_vehicle_prisoner_pod') then
-				hook.Add('Think', veh, function( self )
-					if self:GetInternalVariable( 'm_bEnterAnimOn' ) then
-						hook.Remove( 'Think', self )
-					else
-						if self:GetInternalVariable( 'm_bExitAnimOn' ) then return end
-						self:AddEFlags( EFL_NO_THINK_FUNCTION )
-						hook.Remove( 'Think', self )
-					end
-				end)
-			end
+		hook.Add( "PlayerLeaveVehicle", addonName .. " - Pod fix", function( _, veh )
+			if veh:GetClass() ~= "prop_vehicle_prisoner_pod" then return end
+			hook.Add( "Think", veh, function( self )
+				hook.Remove( "Think", self )
+
+				if self:GetInternalVariable( "m_bEnterAnimOn" ) then return end
+				if self:GetInternalVariable( "m_bExitAnimOn" ) then return end
+				self:AddEFlags( EFL_NO_THINK_FUNCTION )
+			end )
 		end )
 
 	end
 
 	-- Fix for https://github.com/Facepunch/garrysmod-issues/issues/2447
 	-- https://github.com/SuperiorServers/dash/blob/master/lua/dash/extensions/player.lua#L44-L57
-	local ENTITY, PLAYER = FindMetaTable( 'Entity' ), FindMetaTable( 'Player' )
+	local ENTITY, PLAYER = FindMetaTable( "Entity" ), FindMetaTable( "Player" )
 
 	do
 
@@ -274,7 +264,7 @@ if SERVER then
 			positions[ self ] = pos
 		end
 
-		hook.Add( 'FinishMove', addonName .. ' - SetPos Fix', function( ply )
+		hook.Add( "FinishMove", addonName .. " - Player:SetPos fix", function( ply )
 			local pos = positions[ ply ]
 			if not pos then return end
 
@@ -290,25 +280,21 @@ end
 
 if CLIENT then
 
-	local string_StartsWith = string.StartWith or string.StartsWith
-	local string_sub = string.sub
-
 	-- Bind Fix
 	do
 
-		local hookName = addonName .. ' - Bind\'s Fix'
+		local hookName = addonName .. " - Bind fix"
 		local bindsPressed = {}
 
-		hook.Add( 'PlayerBindPress', hookName, function( _, bindName, isDown, keyCode )
-			if string_StartsWith( bindName, '+' ) then
-				bindsPressed[ keyCode ] = string_sub( bindName, 2, #bindName )
-			end
+		hook.Add( "PlayerBindPress", hookName, function( _, bindName, isDown, keyCode )
+			if not string.StartsWith( bindName, "+" ) then return end
+			bindsPressed[ keyCode ] = string.sub( bindName, 2, #bindName )
 		end )
 
-		hook.Add( 'PlayerButtonUp', hookName, function( ply, keyCode )
+		hook.Add( "PlayerButtonUp", hookName, function( ply, keyCode )
 			local bindName = bindsPressed[ keyCode ]
-			if (bindName == nil) or not IsFirstTimePredicted() then return end
-			hook.Run( 'PlayerBindPress', ply, '-' .. bindName, true, keyCode )
+			if not bindName or not IsFirstTimePredicted() then return end
+			hook.Run( "PlayerBindPress", ply, "-" .. bindName, true, keyCode )
 			bindsPressed[ keyCode ] = nil
 		end )
 
@@ -317,27 +303,27 @@ if CLIENT then
 	-- Look Up/Down Fix
 	do
 
-		local hookName = addonName .. ' - Look up/down Fix'
-		local cl_pitchspeed = GetConVar( 'cl_pitchspeed' )
+		local hookName = addonName .. " - Look up/down fix"
+		local cl_pitchspeed = GetConVar( "cl_pitchspeed" )
 		local lookDown = false
 		local lookUp = false
 
-		hook.Add( 'PlayerBindPress', hookName, function( ply, bindName, isDown )
-			if isDown then
-				local bind = string_sub( bindName, 2, #bindName )
-				if (bind == 'lookdown') then
-					lookDown = string_sub( bindName, 1, 1 ) == '+'
-					return true
-				elseif (bind == 'lookup') then
-					lookUp = string_sub( bindName, 1, 1 ) == '+'
-					return true
-				end
+		hook.Add( "PlayerBindPress", hookName, function( ply, bindName, isDown )
+			if not isDown then return end
+
+			local bind = string.sub( bindName, 2, #bindName )
+			if bind == "lookdown" then
+				lookDown = string.sub( bindName, 1, 1 ) == "+"
+				return true
+			elseif bind == "lookup" then
+				lookUp = string.sub( bindName, 1, 1 ) == "+"
+				return true
 			end
 		end )
 
 		local system_HasFocus = system.HasFocus
 
-		hook.Add( 'Think', hookName, function()
+		hook.Add( "Think", hookName, function()
 			if not system_HasFocus() then
 				lookDown = false
 				lookUp = false
@@ -365,16 +351,19 @@ if CLIENT then
 
 	end
 
-	-- speeding up LocalPlayer
+	-- Speeding up LocalPlayer
 	do
 
+		local localPlayer = LocalPlayer
 		local ply
-		local _LocalPlayer = LocalPlayer
 
-		function LocalPlayer()
-			ply = _LocalPlayer()
+		function _G.LocalPlayer()
+			ply = localPlayer()
+
 			if IsValid( ply ) then
-				LocalPlayer = function() return ply end
+				_G.LocalPlayer = function()
+					return ply
+				end
 			end
 
 			return ply
@@ -384,4 +373,4 @@ if CLIENT then
 
 end
 
-MsgC( SERVER and Color( 50, 100, 250 ) or Color( 250, 100, 50 ), string.format( '[%s v%s] ', addonName, version ), color_white, 'Game Patched!\n' )
+MsgC( SERVER and Color( 50, 100, 250 ) or Color( 250, 100, 50 ), string.format( "[%s v%s] ", addonName, version ), color_white, "Game Patched!\n" )
